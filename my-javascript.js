76da448318b724429f773503a9c19b730d4016d0
arjun.ashok <arjun.ashok.it@gmail.com>
2022-10-14 07:51:52 +0530

    Initial upload

---
+++ my-javascript.js
@@ -0,0 +1,72 @@
+
+
+// (ctrl + /) for commenting out a selected block of code
+
+// console.error('Error!')
+
+/*
+console.warn('I am an error')
+console.error('Error!')
+console.log("HI");
+*/
+
+// var javascript = 99;
+// javascript = 100
+// javascript = 999
+// javascript = "Scripting language"
+
+// const javascript = "Some text";
+// let javascript = 99;
+// javascript = 100;
+// javascript = 101;
+// javascript = "Hundred and two"
+
+
+
+// let x = 89;
+// x = 77;
+// x = 90;
+// x = "Hundred"
+// console.log(x);
+
+// const y = 200;
+// console.log(y);
+
+// Data type in JS
+
+// const z = 'This is a constant!'
+// console.log(z);
+
+// const a = 10.89;
+// console.log(a);
+
+// Boolean
+// const truthyValue = true;
+// console.log(truthyValue);
+
+// const falsyValue = false;
+// console.log(falsyValue);
+
+// const x = null;
+// console.log(x);
+
+// let x;
+// console.log(x);
+
+// let num = "thirty";
+// let course_name = "JavaScript";
+// const student_name = "Ankit";
+
+
+// const x = student_name + " asked a question. This is a " + course_name + " lesson." 
+// console.log(x);
+
+
+// const y = `${student_name} asked a question. This is a ${course_name} lesson.`
+// console.log(y);
+
+
+const text = "    COURSE   "
+
+const modified_text = text.toLowerCase().trim().toUpperCase()
+console.log(modified_text);

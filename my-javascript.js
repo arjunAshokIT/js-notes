b381e94890514ea52e5f57d88dd1fabc1ba6b29d
arjun.ashok <arjun.ashok.it@gmail.com>
2022-10-15 07:55:49 +0530

    october-15

--- my-javascript.js
+++ my-javascript.js
@@ -1,5 +1,3 @@
-
-
 // (ctrl + /) for commenting out a selected block of code
 
 // console.error('Error!')
@@ -20,8 +18,6 @@ console.log("HI");
 // javascript = 100;
 // javascript = 101;
 // javascript = "Hundred and two"
-
-
 
 // let x = 89;
 // x = 77;
@@ -57,16 +53,75 @@ console.log("HI");
 // let course_name = "JavaScript";
 // const student_name = "Ankit";
 
-
-// const x = student_name + " asked a question. This is a " + course_name + " lesson." 
+// const x = student_name + " asked a question. This is a " + course_name + " lesson."
 // console.log(x);
 
-
-// const y = `${student_name} asked a question. This is a ${course_name} lesson.`
+// const y = `${student_name} asked a question. This is a ${course_name} lesson.`;
 // console.log(y);
 
+// const text = "    COURSE   "
 
-const text = "    COURSE   "
+// let modified_text = text.toLowerCase().trim().toUpperCase()
 
-const modified_text = text.toLowerCase().trim().toUpperCase()
-console.log(modified_text);
+// let a = text.toLowerCase();
+// let b = a.trim();
+// let c = b.toUpperCase();
+
+// console.log(modified_text);
+
+// Arrays
+
+// let numbers_array = [1, 2, 4, 8, 10, 12, 14]; // array of numbers
+
+
+// let array_of_strings = ['one', 'two', 'three']; // array of strings
+
+
+// let abc = [true, false, true, true, false];
+
+
+// let arr_of_multiple_types = [1, "Hello", "Bye", true, 32];
+
+
+// console.log(arr_of_multiple_types);
+
+// typeof Function
+
+// let a = 88;
+// let b = 'Hi';
+// let c = false;
+
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// console.log(typeof 66);
+// console.log(typeof [1, 2, 4]);
+
+// let number_array = ["One", "Two", "Three", "Four", 5, false];
+// let array_2 = ['a', 'b', 'c'];
+// console.log(number_array);
+
+// Adding items to the end of an array
+// number_array.push(5);
+// number_array.push(31);
+// number_array.push(true);
+
+
+// Adding items to the start of an array
+// number_array.unshift(5);
+// number_array.unshift(6);
+// number_array.unshift(7);
+
+// let first_element = number_array[0];
+// let second_element  = number_array[1];
+// let third_element = number_array[2];
+// let last_element = number_array[number_array.length - 1];
+
+
+// console.log(third_element);
+
+// // array.indexOf()  - to get the index of an element in an array
+// const index = number_array.indexOf(false);
+// console.log(`Index is ${index}`);
